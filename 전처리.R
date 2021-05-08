42586be25b478754a72cd5a212fed61d9ba06baf
Huni0318 <zlatkd12@naver.com>
2021-05-08 20:41:35 +0900

    Update 전처리.R

--- 전처리.R
+++ 전처리.R
@@ -3,7 +3,9 @@ Sys.setlocale(category = "LC_CTYPE", locale = "C")
 data = read.csv("데이터.csv",header = TRUE)
 Sys.setlocale(category = 'LC_ALL',locale='korean')
 library(dplyr)
-
+library(caret)
+library(RANN)
+library(earth)
 
 data = data[data$h10_g4 < 1990, ] # 90년대 이전 출생자 뽑기
 dim(data)
@@ -48,18 +50,181 @@ str(x)
 
 colSums(is.na(x)) # 
 
-x = subset( x, select = -c(X...h10_id,h10_ind,h10_sn,h10_merkey)) # 가구 패널 ID와 관련된 정보는 특별한 의미가 없다고 생각되어 제거
+x = subset( x, select = -c(X...h10_id,h10_ind,h10_sn,h10_merkey,h1013_4aq15)) # 가구 패널 ID와 관련된 정보는 특별한 의미가 없다고 생각되어 제거, h1013_4aq15 값이 없으므로 제거
 dim(x)
 str(x)
-sort(colSums(is.na(x))) # 결측치 값들을 크기 순으로 정렬
+sort(colSums(is.na(x)),decreasing = T) # 결측치 값들을 크기 순으로 정렬
 sort(colSums(is.na(x)),decreasing = T)[1:10]
 
+?preProcess
+imp.x = preProcess(x, method = c("bagImpute")) # 결측값을 bagImputer로 채움
+dim(imp.x)
+sum(is.na(x))
+
+imp.2 <- predict(imp.x, x)
+dim(imp.2)
+
+sum(is.na(imp.2))
+
+set.seed(1996) 
+intrain<-createDataPartition(y=imp.2$y, p=0.7, list=FALSE) 
+train<-imp.2[intrain, ]
+test<-imp.2[-intrain, ]
+
+### decision tree를 이용한 분석
+library(e1071)
+library(tree)
+library(rpart)
+train$y = as.factor(train$y)
+rpartmod<-rpart(y~. , data=train, method="class")
+plot(rpartmod)
+text(rpartmod)
+
+printcp(rpartmod)
+plotcp(rpartmod)
+ptree<-prune(rpartmod, cp= rpartmod$cptable[which.min(rpartmod$cptable[,"xerror"]),"CP"])
+plot(ptree)
+text(ptree)
+rpartpred<-predict(ptree, test, type='class')
+str(test$y)
+test$y = as.factor(test$y)
+dim(test)
+confusionMatrix(rpartpred, test$y)
+
+
+#### random forest
+library(randomForest)
+
+forest_m = randomForest(y~., data = train)
+forest_m
+forest_m$predicted
+forest_m$importance
+forest_m$mtry
+forest_m$ntree
+
+
+sort(forest_m$importance)
+
+p1002_8aq13 
+p1002_8aq14
+p1002_8aq11
+p1002_8aq8 
+p1002_8aq9
+p10_cp 
+h1010_aq17
+h1008_6aq1
+h1006_39
+h10_flag
+
+train = subset( train, select = -c(p1002_8aq13,p1002_8aq14,p1002_8aq11,p1002_8aq8,p1002_8aq9,p10_cp,h1010_aq17,h1008_6aq1,h1006_39,h10_flag))
+test = subset( test, select = -c(p1002_8aq13,p1002_8aq14,p1002_8aq11,p1002_8aq8,p1002_8aq9,p10_cp,h1010_aq17,h1008_6aq1,h1006_39,h10_flag))
+dim(train)
+
+forest_m = randomForest(y~., data = train)
+forest_m
+forest_m$predicted
+forest_m$importance
+forest_m$mtry
+forest_m$ntree
+
+prd_v <- predict(forest_m, newdata = test, type = 'class')
+sum(prd_v == test$y) / nrow(test) * 100
+
+
+prd_v2 <- predict(forest_m, newdata = train, type = 'class')
+sum(prd_v2 == train$y) / nrow(train) * 100
+
+##### graph
+layout(matrix(c(1,2),nrow=1),width=c(4,1)) 
+par(mar=c(5,4,4,0)) # 오른쪽 마진 제거 
+plot(forest_m)
+par(mar=c(5,0,4,2)) # 왼쪽 마진 제거 
+plot(c(0,1),type="n", axes=F, xlab="", ylab="")
+legend("top", colnames(forest_m$err.rate),col=1:4,cex=0.8,fill=1:4)
+
+
+
+### parameter tuning
+ntree_test <- c()
+
+for (i in 300:500) {
+  
+  m <- randomForest(y ~ . , data=train, ntree = i)
+  
+  val_var <- predict(m, newdata=test, type="class") 
+  
+  ntree_test <- c(ntree_test, sum(val_var == test$y)/nrow(test) * 100)
+  
+}
+
+ntree_test
+
+
+
+# train data에 대한 score 
+
+ntree_train <- c()
+
+for (i in 300:500) {
+  
+  m <- randomForest(y ~ . , data=train, ntree = i)
+  
+  val_var <- predict(m, newdata=train, type="class") 
+  
+  ntree_train <- c(ntree_train, sum(val_var == train$y)/nrow(train) * 100)
+  
+}
+
+ntree_train
 
 
 
 
+# 2) mtry 변화
+
+# test data에 대한 score
+
+mtry_test <- c()
+
+for (i in 1:(length(train)-1)) {
+  
+  m <- randomForest(y ~ . , data=train, mtry = i)
+  
+  val_var <- predict(m, newdata=test, type="class") 
+  
+  mtry_test <- c(mtry_test, sum(val_var == test$y)/nrow(test) * 100)
+  
+}
+
+mtry_test
 
 
+
+
+# train data에 대한 score 
+
+mtry_train <- c()
+
+for (i in 1:(length(train)-1)) {
+  
+  m <- randomForest(y ~ . , data=train, mtry = i)
+  
+  val_var <- predict(m, newdata=train, type="class") 
+  
+  mtry_train <- c(mtry_train, sum(val_var == train$y)/nrow(train) * 100)
+  
+}
+
+mtry_train
+
+
+plot(1:length(ntree_test), ntree_test, type = 'l', xlab = 'ntree(size of tree)', col='red', ylim = c(95,100))
+
+lines(1:length(ntree_train), ntree_train, type = 'l')
+
+plot(1:length(mtry_test)-1, mtry_test, type = 'b', xlab = 'mtry', col='red', ylim = c(95,100))
+
+lines(1:length(mtry_train)-1, mtry_train, type = 'b')
 
 
 
